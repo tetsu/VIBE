a6ebae5ca0643b37a20eb27d943574275fd1c395
Tetsuro Mori <tetsu@users.noreply.github.com>
2020-11-17 17:43:33 +0900

    Update install_conda.sh

--- scripts/install_conda.sh
+++ scripts/install_conda.sh
@@ -11,7 +11,8 @@ conda activate $CONDA_ENV_NAME
 which python
 which pip
 
-conda install cudatoolkit=10.1 cudnn=7.6.0
-pip install -c numpy==1.17.5 torch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1
+# conda install cudatoolkit=10.1 cudnn=7.6.0
+# pip install -c numpy==1.17.5 torch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1
+pip install -c numpy==1.17.5 torch==1.4.0 torchvision==0.5.0
 pip install git+https://github.com/giacaglia/pytube.git --upgrade
 pip install -r requirements.txt
